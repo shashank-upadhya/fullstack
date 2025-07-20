aaa1398ed75b0840bd3c62ceda91a8e2813f83f3
Shashank Upadhya <shashankupadhya813@gmail.com>
2025-07-20 17:21:15 +0530

    part3 exercises learning

---
+++ part3/learning/index.js
@@ -0,0 +1,102 @@
+const express= require('express')
+// const http =require('http')
+const app=express()
+const cors=require('cors')
+app.use(cors)
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+
+// const app=http.createServer((request,response)=>{
+//     response.writeHead(200,{'Content-Type':'application/json'})
+//     response.end(JSON.stringify(notes))
+// })
+
+app.use(express.json()) //To access data easily Express json-parser
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
+
+app.get('/',(request,response)=>{
+  response.send('<h1>Hello World</>')
+})
+
+app.get('/api/notes/',(request,response)=>{
+  response.json(notes)
+})
+
+app.get('/api/notes/:id',(request,response)=>{
+  const id=request.params.id
+  const note=notes.find(note=>note.id===id)
+  if (note){
+    response.json(note)
+  } else{
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id',(request,response)=>{
+  const id=request.params.id
+  notes=notes.filter(note=>note.id!==id)
+  response.status(204).end()
+})
+
+const generatedId=()=>{
+  const maxId=notes.length>0
+    ? Math.max(...notes.map(n=>Number(n.id))) : 0
+
+  return String(maxId+1)
+}
+
+app.post('/api/notes/',(request,response)=>{
+  const body=request.body
+  
+  if(!body.content){
+    return response.status(400).json({
+      error:'content is missing'
+    })
+  }
+
+  const note={
+    content:body.content,
+    importnant:body.important||false,
+    id:generatedId()
+  }
+  
+  notes=notes.concat(note)
+
+  console.log(note)
+  response.json(note)
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT=3001
+app.listen(PORT,()=>{
+  console.log(`Server running on PORT ${PORT}`)
+})
---
+++ part3/phonebook/index.js
@@ -0,0 +1,229 @@
+const express=require('express')
+const morgan=require('morgan')
+const app=express()
+
+let persons=[
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.use(express.json())
+
+
+morgan.token('body',(request,response)=>{
+  return request.method === 'POST' ? JSON.stringify(response.body) : 0
+})
+
+// app.use(morgan('tiny: body'))
+
+app.get('/',(request,response)=>{
+  response.send('<h1>Hello World</>')
+})
+
+app.get('/api/persons/',(request,response)=>{
+  response.json(persons)
+})
+
+app.get('/info',(request,response)=>{
+    const n=persons.length
+    const now= new Date()
+    response.send(
+      `<p>Phonebook has a info for ${n} people</p>
+      <p>${now}</p>`
+    )
+})
+
+app.get('/api/persons/:id',(request,response)=>{
+  const id=request.params.id
+  const person=persons.find(person=>person.id===id)
+
+  if(person){
+    response.json(person)
+  }else{
+    response.status(404).end()
+  }
+})
+
+const generatedId=()=>{
+  const maxId=Math.floor(Math.random() * 10000)
+  return String(maxId)
+}
+
+app.post('/api/persons/',(request,response)=>{
+  const body=request.body
+
+  if(!body.name){
+    return response.status(400).json({
+      error:'name is missing'
+    })
+  }
+
+  if(!body.number){
+    return response.status(400).json({
+      error:'number is missing'
+    })
+  }
+
+  const existingPerson = persons.find(person => 
+    person.name.toLowerCase() === body.name.toLowerCase()
+  )
+
+  if(existingPerson){
+    return response.status(400).json({
+      error:"name must be unique"
+    })
+  }
+
+  const person={
+    id:generatedId(),
+    name:body.name,
+    number:body.number
+  }
+
+  persons=persons.concat(person)
+
+  console.log(person)
+  response.json(person)
+})
+
+app.delete('/api/persons/:id',(request,response)=>{
+  const id=request.params.id
+  persons=persons.filter(person=>person.id!==id)
+  response.status(204).end()
+})
+
+const PORT=3001
+app.listen(PORT,()=>{
+    console.log(`Server running on PORT ${PORT}`)
+})
+
+
+// const express = require('express')
+// const app = express()
+
+// let persons = [
+//     { 
+//       "id": "1",
+//       "name": "Arto Hellas", 
+//       "number": "040-123456"
+//     },
+//     { 
+//       "id": "2",
+//       "name": "Ada Lovelace", 
+//       "number": "39-44-5323523"
+//     },
+//     { 
+//       "id": "3",
+//       "name": "Dan Abramov", 
+//       "number": "12-43-234345"
+//     },
+//     { 
+//       "id": "4",
+//       "name": "Mary Poppendieck", 
+//       "number": "39-23-6423122"
+//     }
+// ]
+
+// app.use(express.json())
+
+// app.get('/', (request, response) => {
+//   response.send('<h1>Hello World</h1>')
+// })
+
+// app.get('/api/persons/', (request, response) => {
+//   response.json(persons)
+// })
+
+// app.get('/info', (request, response) => {
+//     const n = persons.length
+//     const now = new Date()
+//     response.send(
+//       `<p>Phonebook has info for ${n} people</p>
+//       <p>${now}</p>`
+//     )
+// })
+
+// app.get('/api/persons/:id', (request, response) => {
+//   const id = request.params.id
+//   const person = persons.find(person => person.id === id)
+
+//   if (person) {
+//     response.json(person)
+//   } else {
+//     response.status(404).end()
+//   }
+// })
+
+// const generatedId = () => {
+//   const maxId = Math.floor(Math.random() * 10000)
+//   return String(maxId)
+// }
+
+// app.post('/api/persons/', (request, response) => {
+//   const body = request.body
+
+//   // Check if name is missing
+//   if (!body.name) {
+//     return response.status(400).json({
+//       error: 'name is missing'
+//     })
+//   }
+
+//   // Check if number is missing
+//   if (!body.number) {
+//     return response.status(400).json({
+//       error: 'number is missing'
+//     })
+//   }
+
+//   // Check if name already exists (case-insensitive comparison)
+//   const existingPerson = persons.find(person => 
+//     person.name.toLowerCase() === body.name.toLowerCase()
+//   )
+
+//   if (existingPerson) {
+//     return response.status(400).json({
+//       error: 'name must be unique'
+//     })
+//   }
+
+//   const person = {
+//     id: generatedId(),
+//     name: body.name,
+//     number: body.number
+//   }
+
+//   persons = persons.concat(person)
+
+//   console.log(person)
+//   response.json(person)
+// })
+
+// app.delete('/api/persons/:id', (request, response) => {
+//   const id = request.params.id
+//   persons = persons.filter(person => person.id !== id)
+//   response.status(204).end()
+// })
+
+// const PORT = 3001
+// app.listen(PORT, () => {
+//     console.log(`Server running on PORT ${PORT}`)
+// })
