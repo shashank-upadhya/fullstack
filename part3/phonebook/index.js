fb0b90ed5989d1d3b8397323ded1cd6992358ef4
Shashank Upadhya <shashankupadhya813@gmail.com>
2025-07-29 21:24:31 +0530

    Exercise of part3-3.9

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,10 @@
 const express=require('express')
 const morgan=require('morgan')
+const cors=require('cors')
+
 const app=express()
+app.use(cors())
+
 
 let persons=[
     { 
@@ -26,7 +30,7 @@ let persons=[
 ]
 
 app.use(express.json())
-
+app.use(express.static('dist'))
 
 morgan.token('body',(request,response)=>{
   return request.method === 'POST' ? JSON.stringify(response.body) : 0
